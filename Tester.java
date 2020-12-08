15c6966dc22081656ba5d99df329cb1c0d2f8341
Logan <lruzzier20@stuy.edu>
2020-12-07 22:09:04 -0500

    stole a test case from mr. k

--- Tester.java
+++ Tester.java
@@ -1,5 +1,13 @@
 public class Tester{
   public static void main(String[] args){
-    Range a = new Range(0,0);
+    Range a = new Range(0,25);
+// shamelessly stolen test case
+        while(a.hasNext()){
+          System.out.print(a.next());
+          if(a.hasNext()){
+             System.out.print(", ");
+          }
+        }
+        System.out.println();
   }
 }
