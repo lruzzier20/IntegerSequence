536aa53f2b584acd34e528314258c51cfa811cd7
Logan <lruzzier20@stuy.edu>
2020-12-08 11:31:40 -0500

    added hasNext()

--- ArraySequence.java
+++ ArraySequence.java
@@ -6,5 +6,11 @@ public class ArraySequence implements IntegerSequence{
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int[] other){
     data=other;
+    currentIndex=0;
+  }
+
+  public boolean hasNext(){
+    if(currentIndex>=data.length){return false;}
+    return true;
   }
 }
